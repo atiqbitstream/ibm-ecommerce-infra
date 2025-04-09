3bf6eeb5e15842286d7264719cece915aa98ad27
atiqbitstream <atiq.bitstream@gmail.com>
2025-04-09 10:40:31 +0500

    Added instance template and auto-scaling group for web servers with Ubuntu image, SSH key, and scaling configuration.

---
+++ auto_scaling.tf
@@ -0,0 +1,21 @@
+resource "ibm_is_instance_template" "web_template" {
+  name = "web_template"
+  image = data.ibm_is_image.ubuntu_image.id
+  profile = "cx2-2x4"
+  keys = [ibm_is_ssh_key.ssh_key.id]
+  resource_group = data.ibm_resource_group.default.id
+
+
+  primary_network_interface{
+    subnet = ibm_is_subnet.ecommmerce_subnet.id
+  }
+}
+
+resource "ibm_is_auto_scaling" "web_asg" {
+  name = "web-asg"
+  instance_template = ibm_is_instance_template.web_template.id
+  subnets = [ibm_is_subnet.ecommmerce_subnet.id]
+  min_members = 2
+  max_members = 5
+  cooldown = 300
+}
